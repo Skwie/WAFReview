7dcdfabc0ba6cd9894b0cca901ad92cfcadc5272
Jordy Groenewoud <jordy.groenewoud@wortell.nl>
2024-09-17 10:53:11 +0200

    Simplify error handling check

--- WAFAzCli.ps1
+++ WAFAzCli.ps1
@@ -1312,11 +1312,11 @@ foreach ($sub in $AllSubscriptions) {
                     $appServiceControlArray[3].Result = 100
                 }
                 catch {
-                    if ($_.Exception.Message.ToString() -like 'The remote server returned an error: (404) Not Found.') {
+                    if ($_.Exception.Response.StatusCode -match 'NotFound') {
                         $tempAppServiceResults += "Bad: Backup and Restore is NOT configured for App Service $($appservice.name)"
                         $appServiceControlArray[3].Result = 0
                     }
-                    elseif ($_.Exception.Message.ToString() -like 'The remote server returned an error: (403) Forbidden.') {
+                    elseif ($_.Exception.Response.StatusCode -match 'Forbidden') {
                         $tempAppServiceResults += "Informational: We have insufficient permissions on App Service $($appservice.name) to evaluate Backup and Restore."
                         $appServiceControlArray[3].Result = 0
                     }
